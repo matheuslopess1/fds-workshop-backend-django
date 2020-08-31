1f20d3d67e4a260fc22244427bf1011938fb25d6
Matheus Inocêncio Lopes <matheuslopes1@protonmail.com>
2020-08-31 18:38:50 -0300

    adiciona PersonDetailView

--- project/people/views.py
+++ project/people/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Person
 
 
 class PersonListView(ListView):
+    model = Person
+
+
+class PersonDetailView(DetailView):
     model = Person
