00d291da274a142026a4e540f3468b9bbb8081ae
Matheus Inocêncio Lopes <matheuslopes1@protonmail.com>
2020-08-31 18:51:43 -0300

    adiciona parâmetro name nos urls de people

--- project/people/urls.py
+++ project/people/urls.py
@@ -3,6 +3,6 @@ from .views import PersonListView, PersonDetailView
 
 
 urlpatterns = [
-    path('', PersonListView.as_view()),
-    path('<int:pk>/', PersonDetailView.as_view())
+    path('', PersonListView.as_view(), name='people_person_list_view'),
+    path('<int:pk>/', PersonDetailView.as_view(), name='people_person_detail_view')
 ]
