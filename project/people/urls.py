ef2d9738325b579749e987b42cb9a49de821a50b
Matheus Inocêncio Lopes <matheuslopes1@protonmail.com>
2020-09-01 20:10:05 -0300

    adiciona / no final das rotas

--- project/people/urls.py
+++ project/people/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', person_list_view, name='people_person_list_view'),
     path('<int:pk>/', person_detail_view, name='people_person_detail_view'),
     path('create/', PersonCreateView.as_view(), name='people_person_create_view'),
-    path('<int:pk>/update', PersonUpdateView.as_view(), name='people_person_update_view'),
-    path('<int:pk>/delete', PersonDeleteView.as_view(), name='people_person_delete_view')
+    path('<int:pk>/update/', PersonUpdateView.as_view(), name='people_person_update_view'),
+    path('<int:pk>/delete/', PersonDeleteView.as_view(), name='people_person_delete_view')
 ]
